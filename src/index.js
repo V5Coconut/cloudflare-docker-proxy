9d083be227cb0f79726164097707eec96e164a2e
V5Coconut <137106716@qq.com>
2024-08-12 10:05:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.v5coconut.xyz.com": "https://registry-1.docker.io",
+  "quay.v5coconut.xyz.com": "https://quay.io",
+  "gcr.v5coconut.xyz.com": "https://gcr.io",
+  "k8s-gcr.v5coconut.xyz.com": "https://k8s.gcr.io",
+  "k8s.v5coconut.xyz.com": "https://registry.k8s.io",
+  "ghcr.v5coconut.xyz.com": "https://ghcr.io",
+  "cloudsmith.v5coconut.xyz.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
